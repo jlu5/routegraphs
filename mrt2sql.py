0765e23002d9b850d879e90542cc99056adfb488
James Lu <james@overdrivenetworks.com>
2023-07-22 17:35:09 -0700

    Switch to pybgpkit_parser

--- mrt2sql.py
+++ mrt2sql.py
@@ -1,36 +1,11 @@
 #!/usr/bin/env python3
 """Export BGP routes info from MRT dumps into a SQLite DB for easier querying."""
 import argparse
-import enum
 import ipaddress
 import pathlib
 import sqlite3
 
-import mrtparse
-
-# https://datatracker.ietf.org/doc/html/rfc6396#section-5.3
-class EntryType(enum.Enum):
-    TABLE_DUMP_V2 = 13
-
-class EntrySubType(enum.Enum):
-    PEER_INDEX_TABLE = 1
-    RIB_IPV4_UNICAST = 2
-    RIB_IPV4_MULTICAST = 3
-    RIB_IPV6_UNICAST = 4
-    RIB_IPV6_MULTICAST = 5
-    RIB_GENERIC = 6
-    RIB_IPV4_UNICAST_ADDPATH = 8
-    RIB_IPV4_MULTICAST_ADDPATH = 9
-    RIB_IPV6_UNICAST_ADDPATH = 10
-    RIB_IPV6_MULTICAST_ADDPATH = 11
-    RIB_GENERIC_ADDPATH = 12
-
-_ROUTE_SUBTYPES = (
-    EntrySubType.RIB_IPV4_UNICAST.value,
-    EntrySubType.RIB_IPV6_UNICAST.value,
-    EntrySubType.RIB_IPV4_UNICAST_ADDPATH.value,
-    EntrySubType.RIB_IPV6_UNICAST_ADDPATH.value,
-)
+import pybgpkit_parser
 
 _DB_INIT_SCRIPT = pathlib.Path(__file__).parent / 'dbinit.sql'
 def db_init(db_filename):
@@ -42,69 +17,55 @@ def db_init(db_filename):
     return con
 
 def parse_mrt(mrt_filename, dbconn):
-    with open(mrt_filename, 'rb') as f:
-        mrt_reader = mrtparse.Reader(f)
+    mrt_reader = pybgpkit_parser.Parser(mrt_filename)
+    for entry in mrt_reader:
+        if entry['elem_type'] != 'A':
+            continue
 
-        for entry in mrt_reader:
-            data = entry.data
-            if EntryType.TABLE_DUMP_V2.value not in data['type']:
-                continue
-            direct_feed_ases = set()
-            if EntrySubType.PEER_INDEX_TABLE.value in data['subtype']:
-                total_peers = data['peer_count']
-                direct_feed_ases |= {peer['peer_as'] for peer in data['peer_entries']}
-                direct_feed_ases.discard(0)
-                print(f'{mrt_filename} total {total_peers} direct feeds ({len(direct_feed_ases)} unique ASes)')
+        # Feed ASN
+        feed_asn = entry['peer_asn']
+        dbconn.execute("INSERT OR REPLACE INTO ASNs VALUES(?, 1)", (feed_asn,))
 
-                dbconn.executemany("INSERT OR IGNORE INTO ASNs VALUES(:peer_as, TRUE)", data['peer_entries'])
-            if any(subtype in data['subtype'] for subtype in _ROUTE_SUBTYPES):
-                prefix_network = data['prefix']
-                prefix_length = data['length']
+        ipn = ipaddress.ip_network(entry['prefix'])
+        prefix_length = ipn.prefixlen
+        network_address = ipn.network_address
+        broadcast_address = ipn.broadcast_address
+        dbconn.execute("INSERT OR IGNORE INTO Prefixes VALUES(?, ?, ?)",
+            (network_address.packed, prefix_length, broadcast_address.packed))
 
-                ipn = ipaddress.ip_network(f'{prefix_network}/{prefix_length}')
-                network_address = ipn.network_address
-                broadcast_address = ipn.broadcast_address
-                dbconn.execute("INSERT OR IGNORE INTO Prefixes VALUES(?, ?, ?)",
-                    (network_address.packed, prefix_length, broadcast_address.packed))
-
-                for rib_entry in data['rib_entries']:
-                    for path_attribute in rib_entry['path_attributes']:
-                        # AS_PATH=2
-                        if 2 in path_attribute['type']:
-                            as_path = tuple(map(int, path_attribute['value'][0]['value']))
-                            origin_asn = as_path[-1]
-                            #print(f"Adding: {prefix_network}/{prefix_length} PATH {as_path}")
-                            path_id = hash(as_path)
-                            for path_index, asn in enumerate(as_path):
-                                dbconn.execute(
-                                    "INSERT OR IGNORE INTO ASNs VALUES(?, ?)",
-                                    (asn, asn in direct_feed_ases)
-                                )
-                                dbconn.execute(
-                                    "INSERT OR IGNORE INTO Paths VALUES(?, ?, ?)",
-                                    (path_id, path_index, asn)
-                                )
-                                if path_index > 0:
-                                    previous_asn = as_path[path_index-1]
-                                    dbconn.execute(
-                                        "INSERT OR IGNORE INTO NeighbourASNs(receiver_asn, sender_asn) VALUES(?, ?)",
-                                        (previous_asn, asn)
-                                    )
-                                    if origin_asn != asn:
-                                        # If the prefix origin isn't the current ASN, that means this AS provides
-                                        # transit for the one before it in the path
-                                        dbconn.execute(
-                                            "UPDATE NeighbourASNs SET transit=1 WHERE receiver_asn==? AND sender_asn==?",
-                                            (previous_asn, asn)
-                                        )
-                            dbconn.execute(
-                                "INSERT OR IGNORE INTO PrefixPaths VALUES(?, ?, ?)",
-                                (network_address.packed, prefix_length, path_id)
-                            )
-                            dbconn.execute(
-                                "INSERT OR IGNORE INTO PrefixOriginASNs VALUES(?, ?, ?)",
-                                (origin_asn, network_address.packed, prefix_length)
-                            )
+        as_path = tuple(map(int, entry['as_path'].split()))
+        origin_asn = as_path[-1]
+        #print(f"Adding: {prefix_network}/{prefix_length} PATH {as_path}")
+        path_id = hash(as_path)
+        for path_index, asn in enumerate(as_path):
+            dbconn.execute(
+                "INSERT OR IGNORE INTO ASNs VALUES(?, 0)", (asn,)
+            )
+            dbconn.execute(
+                "INSERT OR IGNORE INTO Paths VALUES(?, ?, ?)",
+                (path_id, path_index, asn)
+            )
+            if path_index > 0:
+                previous_asn = as_path[path_index-1]
+                dbconn.execute(
+                    "INSERT OR IGNORE INTO NeighbourASNs(receiver_asn, sender_asn) VALUES(?, ?)",
+                    (previous_asn, asn)
+                )
+                if origin_asn != asn:
+                    # If the prefix origin isn't the current ASN, that means this AS provides
+                    # transit for the one before it in the path
+                    dbconn.execute(
+                        "UPDATE NeighbourASNs SET transit=1 WHERE receiver_asn==? AND sender_asn==?",
+                        (previous_asn, asn)
+                    )
+        dbconn.execute(
+            "INSERT OR IGNORE INTO PrefixPaths VALUES(?, ?, ?)",
+            (network_address.packed, prefix_length, path_id)
+        )
+        dbconn.execute(
+            "INSERT OR IGNORE INTO PrefixOriginASNs VALUES(?, ?, ?)",
+            (origin_asn, network_address.packed, prefix_length)
+        )
     dbconn.commit()
 
 def main():

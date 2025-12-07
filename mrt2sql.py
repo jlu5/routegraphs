c72e711fdb3350498821d0b68f5b3f5a1831890a
James Lu <james@overdrivenetworks.com>
2025-12-07 12:13:27 -0800

    mrt2sql: read as_path[0] instead of peer_asn
    The new GRC is separate from the MRT generating server, so peer_asn is the GRC's ASN instead of the actual peer's.

--- mrt2sql.py
+++ mrt2sql.py
@@ -45,21 +45,10 @@ def parse_mrt(mrt_filename, dbconn, registry_path=None):
     mrt_reader = pybgpkit_parser.Parser(mrt_filename, filters={'type': 'announce'})
     as_names = {}
     for entry in mrt_reader:
-        # Feed ASN
-        feed_asn = entry.peer_asn
-        if registry_path and feed_asn not in as_names:
-            as_names[feed_asn] = get_as_name(registry_path, feed_asn)
-        dbconn.execute("INSERT OR REPLACE INTO ASNs VALUES(?, 1, ?)", (feed_asn, as_names.get(feed_asn, '')))
-
-        prefix = entry.prefix
-        unpacked_cidr = unpack_cidr(prefix)
-        network_address_packed, prefix_length, _ = unpacked_cidr
-        dbconn.execute("INSERT OR IGNORE INTO Prefixes VALUES(?, ?, ?)", unpacked_cidr)
-
-        as_path = entry.as_path.split()
+        as_path_raw = entry.as_path.split()
         as_path_parts = []
         ok = True
-        for path_segment in as_path:
+        for path_segment in as_path_raw:
             try:
                 asn = int(path_segment)
             except ValueError:
@@ -74,6 +63,17 @@ def parse_mrt(mrt_filename, dbconn, registry_path=None):
         if not ok:
             continue
         as_path = tuple(as_path_parts)
+
+        # Feed ASN
+        feed_asn = as_path[0]
+        if registry_path and feed_asn not in as_names:
+            as_names[feed_asn] = get_as_name(registry_path, feed_asn)
+        dbconn.execute("INSERT OR REPLACE INTO ASNs VALUES(?, 1, ?)", (feed_asn, as_names.get(feed_asn, '')))
+
+        prefix = entry.prefix
+        unpacked_cidr = unpack_cidr(prefix)
+        network_address_packed, prefix_length, _ = unpacked_cidr
+        dbconn.execute("INSERT OR IGNORE INTO Prefixes VALUES(?, ?, ?)", unpacked_cidr)
 
         path_id = hash(as_path)
         for path_index, asn in enumerate(as_path):
